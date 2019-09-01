517375b228ca84eb37fdc51ba1d73b08a7224858
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2019-09-01 01:06:30 +0100

    Added Day/Night Mode Functionality

---
+++ js/theme-switch.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    ((localStorage.getItem('themeMode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark-background') & document.querySelector('#switch-button').classList.add('fa-sun') : 
+    document.querySelector('body').classList.remove('dark-background')
+})
+
+function switchTheme() {
+    localStorage.setItem('themeMode', (localStorage.getItem('themeMode') || 'dark') === 'dark' ? 'light' : 'dark');
+    localStorage.getItem('themeMode') === 'dark' ? document.querySelector('body').classList.add('dark-background') & document.querySelector('#switch-button').classList.add('fa-sun') :
+    document.querySelector('body').classList.remove('dark-background') & document.querySelector('#switch-button').classList.remove('fa-sun')
+}
