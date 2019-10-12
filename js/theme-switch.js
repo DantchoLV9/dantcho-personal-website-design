99c9c76b2680f5f6733b4db954d5d85106b36230
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2019-10-12 18:23:54 +0100

    Responsive Navigation Menu Functionality Added

--- js/theme-switch.js
+++ js/theme-switch.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 function switchTheme() {
     localStorage.setItem('themeMode', (localStorage.getItem('themeMode') || 'dark') === 'dark' ? 'light' : 'dark');
-    localStorage.getItem('themeMode') === 'dark' ? document.querySelector('body').classList.add('dark-background') & document.querySelector('#switch-button').classList.add('fa-sun') :
+    localStorage.getItem('themeMode') === 'dark' ? document.querySelector('body').classList.add('dark-background') & document.querySelector('#switch-button').classList.add('fa-sun') & document.querySelector('.burger div').classList.add('dark-background') :
     document.querySelector('body').classList.remove('dark-background') & document.querySelector('#switch-button').classList.remove('fa-sun')
 }
