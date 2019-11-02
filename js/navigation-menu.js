f1732c1a80109c1eba3f2359f2bceb80f7101643
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2019-11-02 15:30:58 +0000

    Added blog page & bug fix
    Fixed a bug where the nav bar would come over the content of the page

--- js/navigation-menu.js
+++ js/navigation-menu.js
@@ -3,6 +3,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links a');
+    const pageContent = document.querySelector('.grid-container');
 
     burger.addEventListener('click', ()=> {
 
@@ -12,9 +13,15 @@ const navSlide = () => {
 
             if (link.style.animation) {
                 link.style.animation = '';
+                if (!Object.is(pageContent, null)) {
+                    pageContent.style.animation = '';
+                }
             }
             else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                if (!Object.is(pageContent, null)) {
+                    pageContent.style.animation = 'movePageContentDown 0.3s ease forwards';
+                }
             }
     
         });
