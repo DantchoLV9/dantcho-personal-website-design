2fa5a3d306d28917266fc9def9a4f4b0adc3c914
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2019-11-10 21:00:14 +0000

    Projects Layout Functionality Added

--- js/projects-layout.js
+++ js/projects-layout.js
@@ -1,3 +1,21 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    ((localStorage.getItem('projectsLayout') || 'grid') === 'grid') ? 
+    
+    document.querySelector('#projects-layout-icon').classList.add('fa-th-list') &
+    document.querySelectorAll('.project').forEach((element) => {
+
+        element.classList.replace('project-list', 'project-grid');
+        document.querySelector('.projects-area').classList.add('grid');
+
+    })
+
+    :
+
+    document.querySelector('#projects-layout-icon').classList.add('fa-th');
+    
+});
+
 const changeLayout = () => {
 
     const layoutButton = document.querySelector('.layout-button');
@@ -14,6 +32,7 @@ const changeLayout = () => {
                 element.classList.replace('project-list', 'project-grid');
                 layoutButtonIcon.classList.replace('fa-th', 'fa-th-list')
                 projectsArea.classList.add('grid');
+                localStorage.setItem('projectsLayout', 'grid');
 
             }
             else if (element.classList.contains('project-grid')) {
@@ -21,6 +40,7 @@ const changeLayout = () => {
                 element.classList.replace('project-grid', 'project-list');
                 layoutButtonIcon.classList.replace('fa-th-list', 'fa-th')
                 projectsArea.classList.remove('grid');
+                localStorage.setItem('projectsLayout', 'list');
 
             }
 
